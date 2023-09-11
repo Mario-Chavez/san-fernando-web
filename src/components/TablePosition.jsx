3023ce55c63c77bdcb6c15ca59076edba19db4b9
Mario-Chavez <marioachavez1@gmail.com>
2023-09-11 16:33:05 -0300

    se agrego las tablas de posiciones

--- src/components/TablePosition.jsx
+++ src/components/TablePosition.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { Card, Col, Container, Image, Row } from "react-bootstrap";
 import logo from "../assets/escudo-svg.png";
+import zonaA from "../assets/table/zona-A.png";
+import zonab from "../assets/table/zona-b.png";
 
 const TablePosition = () => {
     return (
         <div className=" container ">
             <Container className=" text-bg-dark">
-                <Row>
+                <Row className=" ">
+                    <Col>
+                        <Image variant="top" src={zonaA} className="w-100" />
+                    </Col>
+                    <Col>
+                        <Image variant="top" src={zonab} className="w-100" />
+                    </Col>
                     <Col>
                         <Image variant="top" src={logo} style={{ width: "5rem" }} />
                     </Col>
-                    <Col></Col>
-                    <Col></Col>
                 </Row>
             </Container>
         </div>
