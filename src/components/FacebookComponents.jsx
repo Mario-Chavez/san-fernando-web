31104b18b01daaafaf60170588b91f64cc748d1f
Mario-Chavez <marioachavez1@gmail.com>
2023-09-13 18:40:37 -0300

    se agrego las redes sociales y se adapto en columna la seccion de facebook

--- src/components/FacebookComponents.jsx
+++ src/components/FacebookComponents.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Image } from "react-bootstrap";
+import { Col, Image, Row } from "react-bootstrap";
 import phone from "../assets/phone.png";
 const FacebookComponents = () => {
     useEffect(() => {
@@ -24,29 +24,57 @@ const FacebookComponents = () => {
     }, []);
 
     return (
-        <div className="container text-center  position-relative">
-            <div className="mx-auto">
-                <Image src={phone} width={"350px"} />
-                <div
-                    className="fb-page position-absolute"
-                    style={{ top: "40px", left: "30%" }}
-                    data-href="https://www.facebook.com/CASFLEALES"
-                    data-tabs="timeline"
-                    data-width="500"
-                    data-height="600"
-                    data-small-header="false"
-                    data-adapt-container-width="true"
-                    data-hide-cover="false"
-                    data-show-facepile="true"
+        <div className="container text-center my-5 ">
+            <Row className="">
+                <Col
+                    sm={7}
+                    className="d-flex  flex-column justify-content-center align-items-center"
                 >
-                    <blockquote
-                        cite="https://www.facebook.com/CASFLEALES"
-                        className="fb-xfbml-parse-ignore"
-                    >
-                        <a href="https://www.facebook.com/CASFLEALES">verde</a>
-                    </blockquote>
-                </div>
-            </div>
+                    <div className="mb-5">
+                        <h2 className="fs-1 fw-bolder text-success">
+                            {" "}
+                            Seguinos en Facebook{" "}
+                        </h2>
+                    </div>
+                    <div className="fs-4">
+                        Nuestra pasión por el fútbol va más allá de los 90 minutos en la
+                        cancha ; es un vínculo que une corazones y emociones en cada
+                        partido. Pero, ¿qué pasa cuando la emoción no se detiene después
+                        del del partido? Ahí es donde entran en juego nuestras redes
+                        sociales. Si aún no nos sigues en nuestras redes sociales, te
+                        estás perdiendo la oportunidad de vivir la experiencia San
+                        Fernando al máximo. A través de nuestras plataformas en línea, te
+                        ofrecemos una ventana directa al corazón del club, donde las
+                        emociones fluyen sin cesar. No te quedes fuera de la jugada. Únete
+                        a nosotros en las redes sociales y prepárate para vivir el fútbol
+                        como nunca antes.{" "}
+                        <p className="fw-bolder text-success">¡Vamos Los Verdes!</p>
+                    </div>
+                </Col>
+
+                <Col sm={5}>
+                    <div className="position-relative overflow-hidden">
+                        <div
+                            className="fb-page "
+                            data-href="https://www.facebook.com/CASFLEALES"
+                            data-tabs="timeline"
+                            data-width="350"
+                            data-height="800"
+                            data-small-header="false"
+                            data-adapt-container-width="true"
+                            data-hide-cover="false"
+                            data-show-facepile="true"
+                        >
+                            <blockquote
+                                cite="https://www.facebook.com/CASFLEALES"
+                                className="fb-xfbml-parse-ignore"
+                            >
+                                <a href="https://www.facebook.com/CASFLEALES">verde</a>
+                            </blockquote>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
         </div>
     );
 };
