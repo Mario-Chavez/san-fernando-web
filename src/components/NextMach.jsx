0d7af5ac0034aee8bcbee833e1528272a65d19b1
Mario-Chavez <marioachavez1@gmail.com>
2023-09-14 16:48:37 -0300

    se cambio el estilo del componente next match

--- src/components/NextMach.jsx
+++ src/components/NextMach.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import { Card, Col, Container, Image, Row } from "react-bootstrap";
 import sanFernando from "../assets/sanFer-escudo.png";
-import allBoys from "../assets/all-boys.png";
+import Nunorco from "../assets/equipos/Nunorco.png";
 
 const NextMach = () => {
     return (
-        <div className=" container my-3 ">
-            <Container className=" text-bg-dark card-style-shadow">
+        <div className=" container my-3  ">
+            <Container className=" text-bg-dark container-nextMatch">
                 <div className="text-center">
                     <h2 className="my-5">Proximo partido</h2>
                 </div>
                 <div className="my-3 pb-5  ">
                     <div className="d-flex  justify-content-center my-2">
-                        <div className="text-center mt-3">
-                            <Image src={sanFernando} style={{ width: "6rem" }} />
+                        <div className="text-center ">
+                            <Image src={sanFernando} style={{ width: "7rem" }} />
                             <p>San fernando</p>
                         </div>
                         <div className="fs-1 fw-bolder mx-5">vs</div>
                         <div className="text-center">
-                            <Image src={allBoys} style={{ width: "6rem" }} />
+                            <Image src={Nunorco} style={{ width: "6rem" }} />
                             <p>All Boys</p>
                         </div>
                     </div>
