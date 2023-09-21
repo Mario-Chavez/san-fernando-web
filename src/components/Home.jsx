c79546b81eb541949de12c8a9afaf96408edebd2
Mario-Chavez <marioachavez1@gmail.com>
2023-09-21 12:51:23 -0300

    se a todos los archivos el agrego.jsx en el componente home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,9 @@
-import SectionMedia from "./SectionMedia";
-import TablePosition from "./TablePosition";
-import FacebookComponents from "./FacebookComponents";
-import NextMach from "./NextMach";
+import SectionMedia from "./SectionMedia.jsx";
+import TablePosition from "./TablePosition.jsx";
+import FacebookComponents from "./FacebookComponents.jsx";
+import NextMach from "./NextMach.jsx";
 import Camisetas from "./Camisetas.jsx";
-import CarrouselComp from "./common/carrouselComp";
+import CarrouselComp from "./common/carrouselComp.jsx";
 
 const Home = () => {
     return (
